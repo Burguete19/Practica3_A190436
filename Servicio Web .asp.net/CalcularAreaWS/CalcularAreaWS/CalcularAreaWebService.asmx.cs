b5402d1c584d9db8525568c8fa1823be47894400
Burguete19 <89111020+Burguete19@users.noreply.github.com>
2024-03-05 14:53:50 -0600

    Add files via upload

---
+++ Servicio Web .asp.net/CalcularAreaWS/CalcularAreaWS/CalcularAreaWebService.asmx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using CalcularAreaWS;
+
+namespace CalcularAreaWS
+{
+    /// <summary>
+    /// Descripción breve de CalcularAreaWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalcularAreaWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+        [WebMethod]
+        public decimal areacuadrado(int valor1, int valor2)
+        {
+            CalcularAreas c = new CalcularAreas();
+            c.Lado1 = valor1;
+            c.Lado2 = valor2;
+            return c.areacuadrado();
+        }
+        [WebMethod]
+        public double areatriangulo(int B, int H)
+        {
+            CalcularAreas c = new CalcularAreas();
+            return c.areatriangulo(B, H);
+        }
+        [WebMethod]
+        public double areacirculo(int R)
+        {
+            CalcularAreas c = new CalcularAreas();
+            return c.areacirculo(R);
+        }
+    }
+}
---
+++ Servicio Web .asp.net/CalcularAreaWS/CalcularAreaWS/CalcularAreas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalcularAreaWS
+{
+    public class CalcularAreas
+    {
+        public decimal Lado1 { get; set; }
+        public decimal Lado2 { get; set; }
+        public decimal areacuadrado()
+        {
+            return Lado1 * Lado2;
+        }
+
+        public double areatriangulo(int B, int H)
+        {
+            var result = (B * H) / 2;
+            return result;
+        }
+
+        public double areacirculo(int R)
+        {
+            var result = Math.PI * Math.Pow(R, 2);
+            return result;
+        }
+    }
+}
